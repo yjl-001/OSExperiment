a512a578d52af4abf2338d7ef1379758d0ae726e
Yu Jianlong <2063217709@qq.com>
2022-11-01 11:14:41 +0800

    20301148 Yu JianLong 2022/11/01 exp1 println!

--- os/src/main.rs
+++ os/src/main.rs
@@ -2,15 +2,45 @@
 #![no_main]
 
 use core::panic::PanicInfo;
+use core::arch::asm;
+
+const SYSCALL_EXIT: usize = 93;
+const SYSCALL_WRITE: usize = 64;
+
+struct Stdout;
+
+impl Write for Stdout {
+    fn write_str(&mut self, s: &str) -> fmt::Result {
+        sys_write(1, s.as_bytes());
+        Ok(())
+    }
+}
+
+pub fn print(args: fmt::Arguments) {
+    Stdout.write_fmt(args).unwrap();
+}
+
+use core::fmt::{self, Write};
+
+#[macro_export]
+macro_rules! print {
+    ($fmt: literal $(, $($arg: tt)+)?) => {
+        $crate::console::print(format_args!($fmt $(, $($arg)+)?));
+    }
+}
+
+#[macro_export]
+macro_rules! println {
+    ($fmt: literal $(, $($arg: tt)+)?) => {
+        print(format_args!(concat!($fmt, "\n") $(, $($arg)+)?));
+    }
+}
 
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
     loop {}
 }
 
-use core::arch::asm;
-
-const SYSCALL_EXIT: usize = 93;
 
 fn syscall(id: usize, args: [usize; 3]) -> isize {
     let mut ret: isize;
@@ -30,8 +60,15 @@ pub fn sys_exit(xstate: i32) -> isize {
     syscall(SYSCALL_EXIT, [xstate as usize, 0, 0])
 }
 
+pub fn sys_write(fd: usize, buffer: &[u8]) -> isize {
+    syscall(SYSCALL_WRITE, [fd, buffer.as_ptr() as usize, buffer.len()])
+}
+
 #[no_mangle]
 extern "C" fn _start() {
+    println!("Hello, world!");
     sys_exit(9);
 }
+
+
 
