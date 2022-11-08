c154d5bdef919fd31b76ead1e099c069b8dc889a
Yu Jianlong <2063217709@qq.com>
2022-11-08 10:44:26 +0800

    20301148 Yu JianLong 2022/11/08 cargo build --release

---
+++ os/target/riscv64gc-unknown-none-elf/release/deps/os-abd48ba8381d02f4.d
@@ -0,0 +1,5 @@
+/root/project/OSExperiment/os/target/riscv64gc-unknown-none-elf/release/deps/os-abd48ba8381d02f4: src/main.rs
+
+/root/project/OSExperiment/os/target/riscv64gc-unknown-none-elf/release/deps/os-abd48ba8381d02f4.d: src/main.rs
+
+src/main.rs:
---
+++ os/target/riscv64gc-unknown-none-elf/release/os.d
@@ -0,0 +1,1 @@
+/root/project/OSExperiment/os/target/riscv64gc-unknown-none-elf/release/os: /root/project/OSExperiment/os/src/main.rs
