2bfc825d4deb40baab1202bd078145f0a489f05d
Yu Jianlong <2063217709@qq.com>
2022-11-08 12:02:49 +0800

    20301148 Yu JianLong 2022/11/08 finish

--- os/target/riscv64gc-unknown-none-elf/release/deps/os-abd48ba8381d02f4.d
+++ os/target/riscv64gc-unknown-none-elf/release/deps/os-abd48ba8381d02f4.d
@@ -1,5 +1,9 @@
-/root/project/OSExperiment/os/target/riscv64gc-unknown-none-elf/release/deps/os-abd48ba8381d02f4: src/main.rs
+/root/project/OSExperiment/os/target/riscv64gc-unknown-none-elf/release/deps/os-abd48ba8381d02f4: src/main.rs src/console.rs src/lang_items.rs src/sbi.rs src/entry.asm
 
-/root/project/OSExperiment/os/target/riscv64gc-unknown-none-elf/release/deps/os-abd48ba8381d02f4.d: src/main.rs
+/root/project/OSExperiment/os/target/riscv64gc-unknown-none-elf/release/deps/os-abd48ba8381d02f4.d: src/main.rs src/console.rs src/lang_items.rs src/sbi.rs src/entry.asm
 
 src/main.rs:
+src/console.rs:
+src/lang_items.rs:
+src/sbi.rs:
+src/entry.asm:
