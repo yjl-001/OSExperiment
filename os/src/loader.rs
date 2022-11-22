4fd30d202c5c1a48d6022699ae51f95238be866a
Yu Jianlong <2063217709@qq.com>
2022-11-22 10:50:05 +0800

    20301148 YuJianLong 2022/11/22 add os/src/loader.rs

---
+++ os/src/loader.rs
@@ -0,0 +1,48 @@
+use core::arch::asm;
+
+use crate::trap::TrapContext;
+use crate::task::TaskContext;
+use crate::config::*;
+
+#[repr(align(4096))]
+#[derive(Copy, Clone)]
+struct KernelStack {
+    data: [u8; KERNEL_STACK_SIZE],
+}
+
+#[repr(align(4096))]
+#[derive(Copy, Clone)]
+struct UserStack {
+    data: [u8; USER_STACK_SIZE],
+}
+
+static KERNEL_STACK: [KernelStack; MAX_APP_NUM] = [
+    KernelStack { data: [0; KERNEL_STACK_SIZE], };
+    MAX_APP_NUM
+];
+
+static USER_STACK: [UserStack; MAX_APP_NUM] = [
+    UserStack { data: [0; USER_STACK_SIZE], };
+    MAX_APP_NUM
+];
+
+impl KernelStack {
+    fn get_sp(&self) -> usize {
+        self.data.as_ptr() as usize + KERNEL_STACK_SIZE
+    }
+    pub fn push_context(&self, trap_cx: TrapContext, task_cx: TaskContext) -> &'static mut TaskContext {
+        unsafe {
+            let trap_cx_ptr = (self.get_sp() - core::mem::size_of::<TrapContext>()) as *mut TrapContext;
+            *trap_cx_ptr = trap_cx;
+            let task_cx_ptr = (trap_cx_ptr as usize - core::mem::size_of::<TaskContext>()) as *mut TaskContext;
+            *task_cx_ptr = task_cx;
+            task_cx_ptr.as_mut().unwrap()
+        }
+    }
+}
+
+impl UserStack {
+    fn get_sp(&self) -> usize {
+        self.data.as_ptr() as usize + USER_STACK_SIZE
+    }
+}
