4ba176cb87671f27bc647fcba4d25a88b1cf45dd
Yu Jianlong <2063217709@qq.com>
2022-11-22 11:07:55 +0800

    20301148 YuJianLong modify os/src/syscall/mod.rs

--- os/src/syscall/mod.rs
+++ os/src/syscall/mod.rs
@@ -1,6 +1,6 @@
 const SYSCALL_WRITE: usize = 64;
 const SYSCALL_EXIT: usize = 93;
-
+const SYSCALL_YIELD: usize = 124;
 mod fs;
 mod process;
 
@@ -12,5 +12,7 @@ pub fn syscall(syscall_id: usize, args: [usize; 3]) -> isize {
         SYSCALL_WRITE => sys_write(args[0], args[1] as *const u8, args[2]),
         SYSCALL_EXIT => sys_exit(args[0] as i32),
         _ => panic!("Unsupported syscall_id: {}", syscall_id),
+        SYSCALL_YIELD => sys_yield(),
+
     }
 }
