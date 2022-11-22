fc5b9fc9315efbbb15ba04bc6ae7cb90bbd64491
Yu Jianlong <2063217709@qq.com>
2022-11-22 10:56:11 +0800

    20301148 YuJianLong 2022/11/22 modify os/src/task/task.rs

--- os/src/task/task.rs
+++ os/src/task/task.rs
@@ -6,3 +6,14 @@ pub enum TaskStatus {
     Exited,
 }
 
+#[derive(Copy, Clone)]
+pub struct TaskControlBlock {
+    pub task_cx_ptr: usize,
+    pub task_status: TaskStatus,
+}
+
+impl TaskControlBlock {
+    pub fn get_task_cx_ptr2(&self) -> *const usize {
+        &self.task_cx_ptr as *const usize
+    }
+}
