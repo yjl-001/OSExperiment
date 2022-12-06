86359ef46ab83dca9de02d75ca8120cb762a820c
Yu Jianlong <2063217709@qq.com>
2022-12-06 15:06:46 +0800

    20301148 YuJianLong 2022/12/12 finish part8

--- user/src/lib.rs
+++ user/src/lib.rs
@@ -13,20 +13,10 @@ pub mod console;
 mod syscall;
 mod lang_items;
 
-fn clear_bss() {
-    extern "C" {
-        fn start_bss();
-        fn end_bss();
-    }
-    (start_bss as usize..end_bss as usize).for_each(|addr| {
-        unsafe { (addr as *mut u8).write_volatile(0); }
-    });
-}
 
 #[no_mangle]
 #[link_section = ".text.entry"]
 pub extern "C" fn _start() -> ! {
-    clear_bss();
     exit(main());
     panic!("unreachable after sys_exit!");
 }
